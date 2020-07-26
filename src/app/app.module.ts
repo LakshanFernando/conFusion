f37e839093632ae9a990ebdfe3953bd2737f3b2f
Lakshan Fernando <tharindulakshan05@gmail.com>
2020-07-27 00:16:48 +0530

    week two

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatListModule, MatGridListModule, MatButtonModule } from '@angular/material'; 
+import { MatToolbarModule, MatListModule, MatGridListModule, MatButtonModule, MatCardModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishDetailComponent
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -21,9 +36,15 @@ import { DishDetailComponent } from './dishdetail/dishdetail.component';
     MatListModule,   
     MatGridListModule, 
     MatButtonModule,
-    FlexLayoutModule
+    MatCardModule,
+    FlexLayoutModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
